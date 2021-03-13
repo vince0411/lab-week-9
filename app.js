ce8977f3e13c38bd2bfe3949503c71d16bf407f8
Vince Enriquez <0411vince@gmail.com>
2021-03-12 21:11:10 -0800

    Finished lab 9

---
+++ app.js
@@ -0,0 +1,30 @@
+
+/// Question 1: Two variables (let) ///
+let x = 7
+let y = 11
+
+/// Question 1: String (Const) ///
+const name = 'Vince'
+
+/// Question 1: Boolean (Const) ///
+const b = new Boolean(false);
+if (b)  {
+   console.log(b)   
+   }
+   Boolean(false)
+
+/// Question 2: Compare numbers using if conditionals ///
+function testNum(x) {
+    let result;
+    if (x < y) {
+      result = 'they match';
+    } else {
+      result = 'no luck';
+    }
+    return result;
+  }
+
+/// Question 3: Using for loop to loop elements of string ///
+for (const element of name) {
+    console.log(element);
+  }
